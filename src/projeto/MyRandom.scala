49d571d55e8e435fde06c38771870990b745b687
kiko67-1 <130848506+kiko67-1@users.noreply.github.com>
2023-04-17 22:54:27 +0100

    Update MyRandom.scala

--- src/projeto/MyRandom.scala
+++ src/projeto/MyRandom.scala
@@ -1,13 +1,12 @@
 package projeto
 
-
 case class MyRandom(seed: Long) extends Randoms {
-    def nextInt: (Int, Randoms) = {
-      val newSeed = (seed * 0x5DEECE66DL + 0xBL) &
+  def nextInt: (Int, Randoms) = {
+    val newSeed = (seed * 0x5DEECE66DL + 0xBL) &
       0xFFFFFFFFFFFFL
-      val nextRandom = MyRandom(newSeed)
-     val n = (newSeed >>> 16).toInt
-     (n, nextRandom)
+    val nextRandom = MyRandom(newSeed)
+    val n = (newSeed >>> 16).toInt
+    (n, nextRandom)
   }
 
 
