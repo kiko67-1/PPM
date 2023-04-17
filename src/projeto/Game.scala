3051f24ce6eb14bd5cefb16dd07ba2f61d68e3c8
kiko67-1 <130848506+kiko67-1@users.noreply.github.com>
2023-04-17 22:53:18 +0100

    Update Game.scala

--- src/projeto/Game.scala
+++ src/projeto/Game.scala
@@ -1,77 +1,72 @@
 package projeto
 
-import projeto.Game._
+import projeto.Game.player
 import scala.util.Random
-import scala.collection.mutable.Stack
+import scala.collection.immutable.::
 
-case class Game(size: Int){
-  def run()=Game.run()
-  def play()= Game.play(board: Board, player: Cells.cell, row: Int, col: Int)
-  def emptyCells()= Game.emptyCells(board: Board,row: Int, col: Int)
-  def printBoard()= Game.printBoard(board: Board)
-  //def hasContiguousLine()= Game.hasContiguousLine(board: Board, player: Cells.cell)
-  def undo()= Game.undo()
-}
+object Game extends App{
 
-object Game {
-
-  val size=0
   type cells=Cells.cell
   type Board = List[List[Cells.cell]]
-  val board: Board = List.fill(size)(List.fill(size)(Cells.Empty))
-  val updatedBoard = board.updated(row, board(row).updated(col, player))
-  val moveHistory = Stack[(Int, Int)]()
-  val player = Cells.Red
-  val row=size
-  val col=size
+  //val moveHistory = Stack[(Int, Int)]()
+  val player = Cells.Blue
 
-  def run(): Unit = {
-    println("Starting HEX game...")
-    println("You move like this: 3,2 (row 3, column 2")
+  def run(size: Int): Unit = {
+    val board: Board = List.fill(size)(List.fill(size)(Cells.Empty))
+    println("Starting Game... \n")
+    println("You move like this: 3,2 (row 3, column 2)")
     printBoard(board)
-    while(!hasContiguousLine(updatedBoard, player)){
-      println("Your move: ")
-      val move = scala.io.StdIn.readLine()
-      val Array(row1, col1) = move.split(",").map(_.toInt)
-      play(board, Cells.Red, row1, col1)
-      val rand = MyRandom(1)
-      val (row2, col2)= randomMove(board, rand)
-      play(board, Cells.Blue, row2._1, row2._2)
-    }
-    println("Exiting game...")
+    print("\nYour move: ")
+    val move = scala.io.StdIn.readLine()
+    val Array(row1, col1) = move.split(",").map(_.toInt)
+    play(board, Cells.Blue, row1, col1)
+    println("\nExiting game...")
   }
 
- def emptyCells(board:Board,row: Int = 0, col: Int = 0): List[(Int,Int)] = {
-   if(board.isEmpty){
-     List()
-   } else if (board.head.isEmpty) {
-     emptyCells(board.tail, row + 1, 0)
-   } else if (board.head.head == Cells.Empty) {
-     (row, col) :: emptyCells(board.head.tail :: board.tail, row, col + 1)
-   } else {
-     emptyCells(board.head.tail :: board.tail, row, col + 1)
-   }
- }
+  def getEmptyCells(board: Board): List[(Int, Int)] = {
+    board.zipWithIndex.flatMap { case (row, rowIndex) =>
+      row.zipWithIndex.collect { case (cell, colIndex) if cell == Cells.Empty =>
+        (rowIndex, colIndex)
+      }
+    }
+  }
   //tarefa 1
-  def randomMove(board: Board, rand: MyRandom):((Int, Int), MyRandom)= {
-      //val coordenates = emptyCells(board)
-      //val int = coordenates()
-    ((0,1), rand)
+  def randomMove(board: Board, rand: Random): (Int, Int) = {
+    val emptyCells = getEmptyCells(board)
+    val index = rand.nextInt(emptyCells.length)   //corrigir para ter o myRandom
+    val pos= emptyCells(index)
+    pos
   }
 
   //tarefa 2
   def play(board: Board, player: Cells.cell, row: Int, col: Int): Board = {
+    val updatedBoard = board.updated(row, board(row).updated(col, player))
     if (board(row)(col) == Cells.Empty) {
-      moveHistory.push((row, col))
+  //    moveHistory.push((row, col))
+      printBoard(updatedBoard)+println()
       if (hasContiguousLine(updatedBoard, player)) {
         println(s"Player $player wins!")
         updatedBoard
       } else {
-        val nextPlayer = if (player == Cells.Red) Cells.Blue else Cells.Red
-        updatedBoard
+        println("Computer moved: ")
+        val rand = new Random()
+        val (row, col) = randomMove(updatedBoard, rand)
+        val updatedBoard2 = updatedBoard.updated(row, updatedBoard(row).updated(col, Cells.Red))
+        if ( updatedBoard(row)(col) == Cells.Empty ) {
+          printBoard(updatedBoard2) + println()
+          print("\nYour move: ")
+          val move = scala.io.StdIn.readLine()
+          val Array(row1, col1) = move.split(",").map(_.toInt)
+          play(updatedBoard2, Cells.Blue, row1, col1)
+        }
+        updatedBoard2
       }
     } else {
       println("Position already taken!")
+      print("\nYour move: ")
+      val move = scala.io.StdIn.readLine()
+      val Array(row1, col1) = move.split(",").map(_.toInt)
+      play(board, Cells.Blue, row1, col1)
       board
     }
   }
@@ -80,18 +75,22 @@ object Game {
   def printBoard(board: Board): String = {
     def printRow(row: List[Cells.cell]): String = row match {
       case Nil => ""
-      case cell :: tail => s"${printCell(cell)} ${printRow(tail)}"
+      case null :: tail => ""
+      case cell :: tail => s"${print(" ")}${printCell(cell)} ${printRow(tail)}"
     }
 
-    def printCell(cell: Cells.cell): String = cell match {
-      case Cells.Red => "R"
-      case Cells.Blue => "B"
-      case Cells.Empty => "."
+    def printCell(cell: Cells.cell): Unit = cell match {
+      case Cells.Red => print("R")
+      case Cells.Blue => print("B")
+      case Cells.Empty => print("X")
     }
 
-    def printBoardHelper(board: List[List[Cells.cell]]): String = board match {
+    def printBoardHelper(board: Board): String = board match {
       case Nil => ""
-      case row :: tail => s"${printRow(row)}${printBoardHelper(tail)}"
+      case row :: tail =>
+        val i=0
+        s"${println(s" "*(i-Game.board.size)+printRow(row)}${printBoardHelper(tail)}" // ver se consigo implementar as diagonais
+      case _ => ""
     }
     printBoardHelper(board)
   }
@@ -103,13 +102,10 @@ object Game {
   }
 
   //tarefa 5
-  def undo(): Unit = {
-    if (moveHistory.nonEmpty) {
-      val (row, col) = moveHistory.pop()
-      //board = board.updated(row, board(row).updated(col, Cells.Empty))
-      //currentPlayer = if (currentPlayer == Cells.Red) Cells.Blue else Cells.Red
-    } else {
-      println("No moves to undo!")
+
+  def undo(board: Board, moves: List[(Int, Int)]): Board = {
+    moves.foldLeft(board) { case (currentBoard, (row, col)) =>
+      currentBoard.updated(row, currentBoard(row).updated(col, Cells.Empty))
     }
   }
 
