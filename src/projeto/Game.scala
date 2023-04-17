a84e9dfb02af595d1b27a296784efceb4d27115f
Francisco Miguel <orlandomiguel006@gmail.com>
2023-04-17 08:58:12 +0100

    Changes by manel

--- src/projeto/Game.scala
+++ src/projeto/Game.scala
@@ -54,9 +54,64 @@ object Game {
    }
  }
   //tarefa 1
+  def randomMove(board: Board, rand: MyRandom):((Int, Int), MyRandom)= {
+      //val coordenates = emptyCells(board)
+      //val int = coordenates()
+    ((0,1), rand)
+  }
 
-  //def randomMove(board: board, rand: Random):((Int, Int), Random)= {
-   // val a=Int
-  //}
+  //tarefa 2
+  def play(board: Board, player: Cells.cell, row: Int, col: Int): Board = {
+    if (board(row)(col) == Cells.Empty) {
+      moveHistory.push((row, col))
+      if (hasContiguousLine(updatedBoard, player)) {
+        println(s"Player $player wins!")
+        updatedBoard
+      } else {
+        val nextPlayer = if (player == Cells.Red) Cells.Blue else Cells.Red
+        updatedBoard
+      }
+    } else {
+      println("Position already taken!")
+      board
+    }
+  }
+
+  //tarefa 3
+  def printBoard(board: Board): String = {
+    def printRow(row: List[Cells.cell]): String = row match {
+      case Nil => ""
+      case cell :: tail => s"${printCell(cell)} ${printRow(tail)}"
+    }
+
+    def printCell(cell: Cells.cell): String = cell match {
+      case Cells.Red => "R"
+      case Cells.Blue => "B"
+      case Cells.Empty => "."
+    }
+
+    def printBoardHelper(board: List[List[Cells.cell]]): String = board match {
+      case Nil => ""
+      case row :: tail => s"${printRow(row)}${printBoardHelper(tail)}"
+    }
+    printBoardHelper(board)
+  }
+
+  //tarefa 4
+  def hasContiguousLine(board: Board, player: Cells.cell): Boolean = {
+    //por fazer
+    false
+  }
+
+  //tarefa 5
+  def undo(): Unit = {
+    if (moveHistory.nonEmpty) {
+      val (row, col) = moveHistory.pop()
+      //board = board.updated(row, board(row).updated(col, Cells.Empty))
+      //currentPlayer = if (currentPlayer == Cells.Red) Cells.Blue else Cells.Red
+    } else {
+      println("No moves to undo!")
+    }
+  }
 
 }
--- src/projeto/MyRandom.scala
+++ src/projeto/MyRandom.scala
@@ -1,3 +1,15 @@
 package projeto
 
-case class MyRandom()
+
+case class MyRandom(seed: Long) extends Randoms {
+    def nextInt: (Int, Randoms) = {
+      val newSeed = (seed * 0x5DEECE66DL + 0xBL) &
+      0xFFFFFFFFFFFFL
+      val nextRandom = MyRandom(newSeed)
+     val n = (newSeed >>> 16).toInt
+     (n, nextRandom)
+  }
+
+
+
+}
