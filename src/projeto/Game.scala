0aaf6ea0189ea2aa9d085a6739818571f734948a
kiko67-1 <130848506+kiko67-1@users.noreply.github.com>
2023-05-05 23:43:17 +0100

    Update Game.scala
    05/05

--- src/projeto/Game.scala
+++ src/projeto/Game.scala
@@ -20,14 +20,14 @@ object Game extends App{
   }
 
   def getEmptyCells(board: Board): List[(Int, Int)] = { board.zipWithIndex.flatMap {
-      case (row, rowIndex) =>
-        row.zipWithIndex.collect {
+    case (row, rowIndex) =>
+      row.zipWithIndex.collect {
         case (cell, colIndex) if cell == Cells.Empty => (rowIndex, colIndex)
       }
-    }
+  }
   }
   //tarefa 1
-   def randomMove(board: Board, rand: MyRandom): ((Int, Int), MyRandom) = {
+  def randomMove(board: Board, rand: MyRandom): ((Int, Int), MyRandom) = {
     val emptyCells = getEmptyCells(board)
     val (index, nextRand) = rand.nextInt
     val cell = emptyCells(index % emptyCells.length)
@@ -36,13 +36,18 @@ object Game extends App{
   def move(board: Board): Unit = {
     print("\nYour move: ")
     val m = scala.io.StdIn.readLine()
-    if(m>=board.size+""){
-      println("Invalid coordinates! Try again")
-      move(board)
-    }else {
-      val Array(row1, col1) = m.split("").map(_.toInt)
-      play(board, Cells.Blue, row1, col1)
+    if(m.size==2) {
+      val Array(row, col) = m.split("").map(_.toInt)
+
+      if ( row > board.size || col > board.size || row <= 0 || col <= 0 ) {
+        println("Invalid coordinates! Try again")
+        move(board)
+      } else {
+        play(board, Cells.Blue, row, col)
+      }
     }
+    println("Invalid coordinates! Use two numbers")
+    move(board)
   }
   //tarefa 2
   def play(board: Board, player: Cells.cell, row: Int, col: Int): Board = {
@@ -101,13 +106,13 @@ object Game extends App{
 
   //tarefa 4
   def hasContiguousLine(board: Board, player: Cells.cell): Boolean = {
-      //por fazer
+    //por fazer
     false
   }
 
   //tarefa 5
   def undo(board: Board, moves: List[(Int, Int)]): Board = { moves.foldLeft(board) {
-      case (currentBoard, (row, col)) => currentBoard.updated(row, currentBoard(row).updated(col, Cells.Empty)) //tentar implementar dentro do jogo
-    }
+    case (currentBoard, (row, col)) => currentBoard.updated(row, currentBoard(row).updated(col, Cells.Empty)) //tentar implementar dentro do jogo
+  }
   }
 }
